a675bcd11e10ecb39175665aae67fddff950ca7b
Fran <caviglia80@gmail.com>
2023-07-30 19:52:39 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms'; 
 
 import { NavComponent } from '@components/nav/nav.component';
 import { ModalComponent } from '@components/modal/modal.component';
-/* import { MainComponent } from '@pages/main/main.component'; */
+import { MainComponent } from '@pages/main/main.component';
 import { DesarrolloComponent } from '@pages/desarrollo/desarrollo.component';
 import { QaComponent } from '@pages/qa/qa.component';
-import { TitleComponent } from './shared/components/title/title.component';
-import { CarouselComponent } from './shared/components/carousel/carousel.component';
-import { PortafolioComponent } from './shared/components/portafolio/portafolio.component';
-import { ExperienciaLaboralComponent } from './shared/components/experiencia-laboral/experiencia-laboral.component';
-import { PerfilProfesionalComponent } from './shared/components/perfil-profesional/perfil-profesional.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { TopButtonComponent } from './shared/components/top-button/top-button.component';
-import { MainModule } from './pages/main/main.module';
+import { TitleComponent } from '@components/title/title.component';
+import { CarouselComponent } from '@components/carousel/carousel.component';
+import { PortafolioComponent } from '@components/portafolio/portafolio.component';
+import { ExperienciaLaboralComponent } from '@components/experiencia-laboral/experiencia-laboral.component';
+import { PerfilProfesionalComponent } from '@components/perfil-profesional/perfil-profesional.component';
+import { FooterComponent } from '@components/footer/footer.component';
+import { TopButtonComponent } from '@components/top-button/top-button.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ModalComponent,
-/*     MainComponent, */
+    MainComponent,
     DesarrolloComponent,
     QaComponent,
     TitleComponent,
@@ -41,7 +42,7 @@ import { MainModule } from './pages/main/main.module';
     BrowserModule,
     BrowserAnimationsModule,
     ModalModule.forRoot(),
-    MainModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
