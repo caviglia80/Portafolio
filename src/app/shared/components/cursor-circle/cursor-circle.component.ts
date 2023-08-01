6a5a8661a644963c460f6e8b30d37790298eadc1
Fran <caviglia80@gmail.com>
2023-08-01 15:22:30 -0300

    .

--- src/app/shared/components/cursor-circle/cursor-circle.component.ts
+++ src/app/shared/components/cursor-circle/cursor-circle.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit, Query } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursor-circle',
@@ -6,20 +6,23 @@ import { Component, HostListener, OnInit, Query } from '@angular/core';
   styleUrls: ['./cursor-circle.component.css']
 })
 export class CursorCircleComponent implements OnInit {
-  circleX: number = 0;
-  circleY: number = 0;
+  private circles: HTMLElement[] = Array.from(document.getElementsByClassName('cursor') as HTMLCollectionOf<HTMLElement>);
+  public circleX: number = 0;
+  public circleY: number = 0;
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
-    this.circleX = event.pageX;
+    const limit = this.circles[0].getBoundingClientRect().width * 1.4 + event.pageX;
+    if (limit < window.innerWidth)
+      this.circleX = event.pageX;
     this.circleY = event.pageY;
   }
 
   ngOnInit(): void {
-    const circles: HTMLElement[] = Array.from(document.getElementsByClassName('cursor') as HTMLCollectionOf<HTMLElement>);
+    this.circles = Array.from(document.getElementsByClassName('cursor') as HTMLCollectionOf<HTMLElement>);
     document.addEventListener("click", () => {
-      if (circles.length > 0) {
-        circles.forEach(circle => {
+      if (this.circles.length > 0) {
+        this.circles.forEach(circle => {
           circle.classList.add("big-and-fade-in-out");
           circle.addEventListener("animationend", () => {
             circle.classList.remove("big-and-fade-in-out");
