bf12cf1a5054fec72697974cf9b08d4cbc4b6f0c
Francisco <caviglia80@gmail.com>
2024-03-30 12:39:07 +0100

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCDr2K33j7CQaRuncmOSeaM1J8gBdKD6Rs",
+    authDomain: "portafolio-5e1ae.firebaseapp.com",
+    projectId: "portafolio-5e1ae",
+    storageBucket: "portafolio-5e1ae.appspot.com",
+    messagingSenderId: "511057626710",
+    appId: "1:511057626710:web:7000657ba98b116c339df7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCDr2K33j7CQaRuncmOSeaM1J8gBdKD6Rs",
+    authDomain: "portafolio-5e1ae.firebaseapp.com",
+    projectId: "portafolio-5e1ae",
+    storageBucket: "portafolio-5e1ae.appspot.com",
+    messagingSenderId: "511057626710",
+    appId: "1:511057626710:web:7000657ba98b116c339df7"
+  }
 };
 
+// falta npm install @angular/fire firebase --save y modificar app.module ( AngularFireModule.initializeApp(environment.firebaseConfig) // Inicializa Firebase)
